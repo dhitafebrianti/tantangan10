ab7e7f52603d4b7411e82b6b1ce02355a82b4a5f
dhitafebrianti <49123000+dhitafebrianti@users.noreply.github.com>
2019-04-08 20:13:52 +0800

    update bubblesortdsc.cpp
    menganti beberapa di perintah bubblesort supaya yang ditampilkan akan berubah jadi yang terbesar ke terkecil

--- bubblesortdsc.cpp
+++ bubblesortdsc.cpp
@@ -9,9 +9,9 @@ int xpp= *yp;
 void bubblesort(int arr[],int n)
 {
 int i,j;
-for(i=0;i<n-1;i++)
-for(j=0;j<n-1;j++)
-if(arr[j]>arr[j+1])
+for(i=7-1;i>=0;i--)
+for(j=0;j<=n-1;j++)
+if(arr[j]<arr[j+1])
 swap(&arr[j],&arr[j+1]);
 }
 void printarray(int arr[],int size)
